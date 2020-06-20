31a24f13c2388858a684f69fe2389dd1684e042a
Mayconn Oliveira <mayconnwgo@gmail.com>
2020-06-20 10:16:20 -0300

    Hello World

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-const test = () => {};
+import express from 'express';
+import cors from 'cors';
 
-export default test;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/test', (req, res) => {
+  return res.json({ hello: 'world' });
+});
+
+app.listen(process.env.PORT || 3333, () => console.log('Server Started'));
